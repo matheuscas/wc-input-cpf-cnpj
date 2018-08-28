1fb523ec71b6dcb7e8429a0d12d8379261d0991b
Matheus Cardoso <matheuscas@Matheuss-MacBook-Pro.local>
2018-08-27 22:22:14 -0300

    apply mask to content

--- src/components/my-component/my-component.tsx
+++ src/components/my-component/my-component.tsx
@@ -1,4 +1,4 @@
-import { Component, Prop, EventEmitter, Event, State } from '@stencil/core';
+import { Component, Prop, EventEmitter, Event } from '@stencil/core';
 import { CPF_LENGTH, isCPFValid, isCNPJValid } from './validation';
 
 @Component({
@@ -9,7 +9,7 @@ import { CPF_LENGTH, isCPFValid, isCNPJValid } from './validation';
 export class InputCpfCnpj {
 
   @Prop() placeholder?: string;
-  @Prop() value?: string;
+  @Prop({mutable: true}) value?: string;
   @Prop() autocomplete?: string;
   @Prop() maxlength?: string;
   @Prop() minlength?: string;
@@ -24,14 +24,40 @@ export class InputCpfCnpj {
 
   inputChanged(event) {
     const value: string = event.target.value.trim();
+    let cleannedValue = this.removeAnySpaces(value)
+    cleannedValue = this.removePunctuation(cleannedValue)
     let result;
-    if (value.length <= CPF_LENGTH) {
-      result = isCPFValid(value);
+    if (cleannedValue.length <= CPF_LENGTH) {
+      this.value = this.applyCPFMask(cleannedValue)
+      result = isCPFValid(cleannedValue);
     } else {
-      result = isCNPJValid(value);
+      this.value = this.applyCNPJMask(cleannedValue)
+      result = isCNPJValid(cleannedValue);
     }
     this.valid.emit(result);
-    this.isValid = result;
+  }
+
+  removePunctuation(value){
+    return value.replace(/(~|`|!|@|#|$|%|^|&|\*|\(|\)|{|}|\[|\]|;|:|\"|'|<|,|\.|>|\?|\/|\\|\||-|_|\+|=)/g,"");
+  }
+
+  removeAnySpaces(value) {
+    return value.replace(/ +?/g, '');
+  }
+
+  applyCPFMask(value) {
+    const firstPart = value.replace(/^(\d{3})/, '$1.');
+    const secondPart = firstPart.replace(/^(\d{3}.)(\d{3})/, '$1$2.');
+    const thirdPart = secondPart.replace(/^(\d{3}.)(\d{3}.)(\d{3})/, '$1$2$3-');
+    return thirdPart
+  }
+
+  applyCNPJMask(value) {
+    const firstPart = value.replace(/^(\d{2})/, '$1.');
+    const secondPart = firstPart.replace(/^(\d{2}.)(\d{3})/, '$1$2.');
+    const thirdPart = secondPart.replace(/^(\d{2}.)(\d{3}.)(\d{3})/, '$1$2$3/');
+    const fourthPart = thirdPart.replace(/^(\d{2}.)(\d{3}.)(\d{3}\/)(\d{4})/, '$1$2$3$4-');
+    return fourthPart
   }
 
   render() {
